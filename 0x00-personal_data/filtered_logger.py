9668cd2623620dc2b5547214e8ba931d23fc88c8
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-14 12:36:56 +0100

    filter logger - Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -19,14 +20,22 @@ def filter_datum(
     return message
 
 
-"""
-Main file
-"""
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
 
-# fields = ["password", "date_of_birth"]
-# messages = [
-#    "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-#    "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
-#
-# for message in messages:
-#    print(filter_datum(fields, 'xxx', message, ';'))
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """insantiates the RedactingFormatter
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the record
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
