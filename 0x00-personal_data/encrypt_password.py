8e9c86ad7e3733d064cd9966280f62d37483e739
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-15 18:10:24 +0100

    encrypt_password - Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,9 @@ def hash_password(password: str) -> bytes:
     """returns a salted, hashed password
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate that the provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
