250c145d52963ec1d7741e7127ab440a16695365
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-16 11:17:44 +0100

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,13 +2,13 @@
 """Module to Manage the API Authentication
 """
 
-from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
     """Auth class to manage the API authentication
     """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method to require the authentication
         """
@@ -23,7 +23,9 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """Method to get the authorization header
         """
-        return None
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Method to get the current user
