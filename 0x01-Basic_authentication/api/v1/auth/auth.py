753f3104289e4870e8ea3cb73becda6c66ead594
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-16 06:05:07 +0100

    Auth class - required route authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,7 +2,6 @@
 """Module to Manage the API Authentication
 """
 
-
 from flask import request
 from typing import List, TypeVar
 
@@ -13,7 +12,13 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method to require the authentication
         """
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Method to get the authorization header
