ebcce11e067c6eb0b8a64c3d1ef513825e5b90be
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-16 11:33:55 +0100

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,15 @@ class BasicAuth(Auth):
     """BasicAuth class to manage the API authentication
     """
     pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """returns the Base64 part of the Authorization header for a
+            Basic Authentication
+        """
+        if authorization_header is None or not isinstance(
+                authorization_header, str):
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
