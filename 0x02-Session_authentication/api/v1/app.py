8f43b77f10421726daf39043950404ffcdd41668
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-22 02:33:13 +0100

    before request - excluded session_login

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,8 +36,10 @@ def before_request():
     if auth.require_auth(request.path,
                          ['/api/v1/status/',
                           '/api/v1/unauthorized/',
-                          '/api/v1/forbidden/']):
-        if auth.authorization_header(request) is None:
+                          '/api/v1/forbidden/',
+                          '/api/v1/auth_session/login/']):
+        if auth.authorization_header(
+                request) is None and auth.session_cookie(request) is None:
             abort(401)
         if user is None:
             abort(403)
