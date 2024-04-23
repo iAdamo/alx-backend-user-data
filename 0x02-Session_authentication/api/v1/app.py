6ccf1ef97cff472ec6f7d5b9e97212512604f359
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-23 10:55:18 +0100

    Session logout

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,7 +37,8 @@ def before_request():
                          ['/api/v1/status/',
                           '/api/v1/unauthorized/',
                           '/api/v1/forbidden/',
-                          '/api/v1/auth_session/login/']):
+                          '/api/v1/auth_session/login/',
+                          '/api/v1/auth_session/logout/']):
         if auth.authorization_header(
                 request) is None and auth.session_cookie(request) is None:
             abort(401)
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,10 +4,7 @@
 from api.v1.views import app_views
 from flask import jsonify, request, abort
 from models.user import User
-from api.v1.auth.session_auth import SessionAuth
 from os import getenv
-
-AUTH = SessionAuth()
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -29,7 +26,21 @@ def login() -> str:
     user = users[0]
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-    session_id = AUTH.create_session(user.id)
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
     response.set_cookie(getenv('SESSION_NAME'), session_id)
     return response
+
+
+@app_views.route(
+    '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """ DELETE /auth_session/logout
+    Return:
+      - Empty dictionary
+    """
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
