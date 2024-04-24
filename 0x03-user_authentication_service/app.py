d7df774f80aa9e5d3865578be17c707460322594
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-24 15:09:44 +0100

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """Basic Flask app
 """
 
-from flask import Flask, jsonify
+from flask import Flask, abort, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+auth = Auth()
 
 
 @app.route('/')
@@ -12,6 +14,19 @@ def basic_app() -> str:
     """Basic app
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """User registration
+    """
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        auth.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
