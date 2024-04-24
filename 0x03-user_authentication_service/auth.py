0857e5e4b02f87f4bd1a2dd7bc0d9c307b872a78
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-24 15:29:19 +0100

    Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -47,3 +47,14 @@ class Auth:
                 user.hashed_password.encode('utf-8'))
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
