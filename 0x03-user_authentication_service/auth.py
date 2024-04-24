64011e0b0f486376da98af1b52364a587b6e42ef
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-24 20:07:43 +0100

    Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,3 +69,8 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy a session
+        """
+        self._db.update_user(user_id, session_id=None)
