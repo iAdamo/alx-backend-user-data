702955dfc44fa6c675df5d8f8aa558c45f433d06
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-24 02:48:50 +0100

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,3 +41,13 @@ class DB:
         """Find a user by a given attribute
         """
         return self._session.query(User).filter_by(**kwargs).one()
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
